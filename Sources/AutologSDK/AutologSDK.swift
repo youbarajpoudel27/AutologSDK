801535b02be5a1c665d7268b9243eff972d47552
Youb <youbaraj@inarix.com>
2024-02-26 15:45:40 +0100

    Added Core ML SDK

--- Sources/AutologSDK/AutologSDK.swift
+++ Sources/AutologSDK/AutologSDK.swift
@@ -1,2 +1,13 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import SwiftUI
+
+public struct SwiftUIView: View {
+    @available(iOS 13.0, *)
+   public var body: some View {
+        CameraViewSDK()
+    }
+}
+
+ 
